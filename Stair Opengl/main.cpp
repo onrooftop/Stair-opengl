2315ef5d0f99f3c4967daf5b4d3d9ac2fc16872d
panupong <panupong.meta@gmail.com>
2017-08-23 19:51:47 +0700

    setting window in main.cpp

--- Stair Opengl/main.cpp
+++ Stair Opengl/main.cpp
@@ -7,9 +7,50 @@
 #include <GLFW\glfw3.h>
 
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+const char *WINDOW_NAME = "Stair";
+
 
 
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, WINDOW_NAME, NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "FAILED TO CREATE WINDOW\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "FAILED TO LOAD OPENGL\n";
+		return -1;
+	}
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
 	return 0;
 }
