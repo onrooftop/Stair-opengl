825d6e7362ab124f4dffd6c8e40bae734dbfd50b
panupong <panupong.meta@gmail.com>
2017-08-23 23:04:42 +0700

    add vertices vao vbo and draw

--- Stair Opengl/main.cpp
+++ Stair Opengl/main.cpp
@@ -6,6 +6,8 @@
 #include <glad\glad.h>
 #include <GLFW\glfw3.h>
 
+#include "Shader.h"
+
 
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow *window, int w, int h);
@@ -43,13 +45,77 @@ int main()
 	}
 
 
+
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f, // 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  //1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  //1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  //1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f, // 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, // 0.0f, 0.0f,
+							 //
+		-0.5f, -0.5f,  0.5f, // 0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  //1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  //1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  //1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f, // 0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f, // 0.0f, 0.0f,
+							 //
+		-0.5f,  0.5f,  0.5f, // 1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f, // 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, // 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, // 0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f, // 0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f, // 1.0f, 0.0f,
+							 //
+		0.5f,  0.5f,  0.5f,  //1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  //1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  //0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  //0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  //0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  //1.0f, 0.0f,
+							 //
+		-0.5f, -0.5f, -0.5f, // 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  //1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  //1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  //1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f, // 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, // 0.0f, 1.0f,
+							 //
+		-0.5f,  0.5f, -0.5f, // 0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  //1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  //1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  //1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f, // 0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f // 0.0f, 1.0f
+	};
+
+	Shader myShader("vshader.vert", "fshader.frag");
+
+	unsigned int VAO, VBO;
+
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+
+	glEnable(GL_DEPTH_TEST);
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		myShader.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
 		glfwSwapBuffers(window);
